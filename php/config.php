ad071c6b671a7bd6b0cf38c05d588330fceeead0
Ankit Sharma <153091619+ankitsharma785197@users.noreply.github.com>
2024-02-18 16:32:48 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,6 +1,6 @@
 <?php
 #error_reporting(0);
-$conn = mysqli_connect("localhost","u851360990_chat_app","707089@Ankit","u851360990_chat_app");
+$conn = mysqli_connect("localhost","chat_app_db","password@xyz","chat_app_db");
 if(!$conn){
     echo"Success".mysqli_connect_error();
 }
